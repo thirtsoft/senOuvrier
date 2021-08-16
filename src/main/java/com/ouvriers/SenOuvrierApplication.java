0eb5cc6a86fdd37ddd7e1f74b86aad81b8047fab
thirtsoft <thirtsoft@gmail.com>
2021-08-16 16:45:27 +0000

    first commit

---
+++ src/main/java/com/ouvriers/SenOuvrierApplication.java
@@ -0,0 +1,13 @@
+package com.ouvriers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenOuvrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenOuvrierApplication.class, args);
+	}
+
+}
